7011b94b32bab5519b44e7b31c85a52e373a22a1
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2024-09-03 16:24:08 -0400

    Bringing in PE Folder

---
+++ PE/PE_HelloWorld_JRB/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PE_HelloWorld_JRB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Writes strings to the console window. 
+            Console.WriteLine("Hello GDP1!");
+            Console.WriteLine("The sky is blue!");
+            Console.WriteLine("Jonathan Benz");
+        }
+    }
+}
---
+++ PE/PE_StatementsExpressions_JRB/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Jonathan Benz
+ * PE - Statements & Expressions
+ * No known issues
+ */
+namespace PE_StatementsExpressions_JRB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write character name and a blank line for extra space
+            Console.WriteLine("Name: Jonathan Benz");
+            Console.WriteLine();
+
+            // First stat is 20% of 50
+            Console.WriteLine("First Stat: " + 50 * 0.2f);
+            // Second stat is 50% of the First Stat
+            Console.WriteLine("Second Stat: " + (50 * 0.2f) * 0.5f);
+            // Third stat is always 7
+            Console.WriteLine("Third Stat: 7");
+            // Fourth stat is the sum of the second and third states, minus 2
+            Console.WriteLine("Fourth Stat: " + ((((50 * 0.2f) * 0.5f) + 7) - 2));
+            // Fifth stat is 50 - the rest
+            Console.WriteLine("Fifth Stat: " +
+                (50 -
+                    ((50 * 0.2f) +                      // First stat
+                     ((50 * 0.2f) * 0.5f) +             // Second stat
+                     (7) +                              // Third stat
+                     (((((50 * 0.2f) * 0.5f) + 7) - 2)) // Fourth stat
+                     )
+                    )
+                );
+
+            // Put a blank space for extra space and the total points at the end
+            Console.WriteLine();
+            Console.WriteLine("Total Points: 50");
+        }
+    }
+}
