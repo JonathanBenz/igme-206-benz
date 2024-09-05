743893809bc357b961f5599a87c499cd90390556
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2024-09-05 17:56:49 -0400

    Data Types & Variables

---
+++ PE/PE_DataTypesAndVariables_JRB/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Author: Jonathan Benz
+ * PE - Data Types & Variables
+ * No Known Issues */
+namespace PE_DataTypesAndVariables_JRB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring and initializing all variables
+            const int startingPoints = 50;
+            string name = "Jonathan Benz";
+            double strength = startingPoints * 0.23;
+            double dexterity = strength / 2;
+            int intelligence = 7;
+            double health = (dexterity + intelligence) - 2;
+            double charisma = startingPoints - (strength + dexterity + intelligence + health);
+            double totalPoints = strength + dexterity + intelligence + health + charisma;
+
+            // Printing out all the statements
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine();
+
+            Console.WriteLine("Strength: " + strength);
+            Console.WriteLine("Dexterity: " + dexterity);
+            Console.WriteLine("Intelligence: " + intelligence);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Charisma: " + charisma);
+            Console.WriteLine();
+
+            Console.WriteLine("TOTAL: " + totalPoints);
+        }
+    }
+}
