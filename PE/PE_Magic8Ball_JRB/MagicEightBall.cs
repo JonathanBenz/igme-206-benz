e4e9469a04826144e0ee0582f834aeef76061974
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2024-10-01 15:08:23 -0400

    PE_Magic8Ball_JRB

---
+++ PE/PE_Magic8Ball_JRB/MagicEightBall.cs
@@ -0,0 +1,68 @@
+﻿/* Author: Jonathan Benz
+ * Magic Eight Ball Class
+ * No known issues */
+namespace PE_Magic8Ball_JRB
+{
+    internal class MagicEightBall
+    {
+        // Declaring class fields. 
+        private string owner;
+        private string[] responses;
+        private Random rng;
+        private int timesShaken;
+
+        /// <summary>
+        /// Constructor for creating instances of the MagicEightBall class. 
+        /// </summary>
+        /// <param name="owner"> The name of who is using the Magic Eight Ball. </param>
+        public MagicEightBall(string owner)
+        {
+            this.owner = owner;
+            responses = [
+                          "It is certain.",
+                          "As I see it, yes.",
+                          "Concentrate and ask again.",
+                          "Outlook not so good.",
+                          "Very doubtful."
+                        ];
+            rng = new Random();
+            timesShaken = 0;
+        }
+
+        /// <summary>
+        /// Generates a random response. Increments the timeShaken variable. 
+        /// </summary>
+        /// <returns> a random string from the responses string array. </returns>
+        public string ShakeBall()
+        {
+            int randomIndex = rng.Next(0, responses.Length);
+            string answer = String.Format("  > The Magic 8 Ball says: {0}", responses[randomIndex]);
+            timesShaken++;
+            return answer;
+        }
+
+        /// <summary>
+        /// Returns a message based on the amount of times the ball has been shaken. 
+        /// </summary>
+        /// <returns> a string message. </returns>
+        public string Report()
+        {
+            string returnMessage;
+            if(timesShaken < 1)
+            {
+                returnMessage = String.Format("  > {0} has not shaken the ball yet.", owner);
+                return returnMessage;
+            }
+            else if(timesShaken < 4)
+            {
+                returnMessage = String.Format("  > {0} has shaken the ball {1} times.", owner, timesShaken);
+                return returnMessage;
+            }
+            else
+            {
+                returnMessage = String.Format("  > {0} has shaken the ball {1} times. That's a lot of questions!", owner, timesShaken);
+                return returnMessage;
+            }
+        }
+    }
+}
---
+++ PE/PE_Magic8Ball_JRB/Program.cs
@@ -0,0 +1,66 @@
+﻿/* Author: Jonathan Benz
+ * PE - Magic 8 Ball
+ * No known issues */
+namespace PE_Magic8Ball_JRB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring variables
+            string owner;
+            string choice;
+            bool hasQuit = false;
+
+            // Step 1: Getting the owner
+            Console.WriteLine("Welcome to Magic 8 Ball Simulator!");
+            owner = GetInput("  > Who owns this ball?");
+
+            // Step 2: Instantiating MagicEightBall
+            MagicEightBall magicEightBall = new MagicEightBall(owner);
+
+            // Step 3: User controls program choices
+            do
+            {
+                Console.WriteLine("\nWhat would you like to do?");
+                choice = GetInput("You can \'shake\' the ball, get a \'report\', or \'quit\':");
+                choice = choice.ToLower();
+
+                switch(choice)
+                {
+                    case "shake":
+                        // temp variable. We don't need to save this data. 
+                        string question = GetInput("  > What is your question?");
+                        Console.WriteLine(magicEightBall.ShakeBall());
+                        break;
+                    case "report":
+                        Console.WriteLine(magicEightBall.Report());
+                        break;
+                    case "quit":
+                        Console.WriteLine("  > Goodbye!");
+                        hasQuit = true;
+                        break;
+                    default:
+                        Console.WriteLine("  > I do not recognize that response.");
+                        break;
+                }
+            }
+            while (!hasQuit);
+        }
+
+        /// <summary>
+        /// Helper method to handle input.
+        /// </summary>
+        /// <param name="prompt"> string prompt to ask the user. </param>
+        /// <returns> a string of the user input's input trimmed. </returns>
+        private static string GetInput(string prompt)
+        {
+            Console.Write(prompt + " ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            string input = Console.ReadLine();
+            input = input.Trim();
+            Console.ForegroundColor = ConsoleColor.White;
+            return input;
+        }
+    }
+}
