e19e5907a55d272fe3257aaebffb249d514a8c18
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2024-10-02 17:35:37 -0400

    PE_Properties_JRB

---
+++ PE/PE_Properties_JRB/Book.cs
@@ -0,0 +1,72 @@
+﻿/* Author: Jonathan Benz
+ * Book Class
+ * No known issues */
+namespace PE_Properties_JRB
+{
+    internal class Book
+    {
+        // Declaring Class Fields
+        private string title;
+        private string author;
+        private int numPages;
+        private string owner;
+        private int totalTimesRead;
+
+        // Constructor
+        public Book(string title, string author, int numPages, string owner)
+        {
+            this.title = title;
+            this.author = author;
+            this.numPages = numPages;
+            this.owner = owner;
+            totalTimesRead = 0;
+        }
+
+        // Properties
+        public string Title
+        {
+            get { return title; }
+        }
+        public string Author
+        {
+            get { return author; }
+        }
+        public int NumPages
+        {
+            get { return numPages; }
+        }
+
+        public string Owner
+        {
+            get { return owner; }
+            set { 
+                value = value.Trim();
+                if (value != "")
+                    {
+                        owner = value;
+                    }
+                }
+        }
+        public int TotalTimesRead
+        {
+            get { return totalTimesRead; }
+            set
+            {
+                if (value > totalTimesRead)
+                {
+                    totalTimesRead = value;
+                }
+            }
+        }
+
+        // Methods
+        /// <summary>
+        /// Prints information about the book, including it's title, author, number of pages, owner, and total times read. . 
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine("{0} by {1} has {2} pages.", title, author, numPages);
+            Console.WriteLine("It is owned by {0} and has been read {1} times.", owner, totalTimesRead);
+        }
+    }
+}
---
+++ PE/PE_Properties_JRB/Program.cs
@@ -0,0 +1,93 @@
+﻿/* Author: Jonathan Benz
+ * PE - Properties
+ * No known issues */
+namespace PE_Properties_JRB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring variables
+            string title;
+            string author;
+            int numPages;
+            string owner;
+            int totalTimesRead;
+
+            string userInput;
+            bool isDone = false;
+
+            // Prompting for input
+            Console.WriteLine("Welcome to Book Simulator 2024\n");
+
+            Console.Write("What is the book's title? ");
+            title = Console.ReadLine();
+
+            Console.Write("Who is the book's author? ");
+            author = Console.ReadLine();
+
+            Console.Write("How many pages does it have? ");
+            numPages = int.Parse(Console.ReadLine());
+
+            Console.Write("Who is the book's current owner? ");
+            owner = Console.ReadLine();
+
+            // Instantiating the Book
+            Book book = new Book(title, author, numPages, owner);
+
+            // Looping for input
+            do
+            {
+                Console.Write("\nWhat would you like to do? ");
+                userInput = Console.ReadLine();
+
+                switch(userInput)
+                {
+                    case "title":
+                        Console.WriteLine("The title is {0}.", book.Title);
+                        break;
+                    case "author":
+                        Console.WriteLine("The author is {0}.", book.Author);
+                        break;
+                    case "pages":
+                        Console.WriteLine("The book has {0} pages.", book.NumPages);
+                        break;
+                    case "owner":
+                        Console.Write("Would you like to change the owner (yes/no)? ");
+                        string answer = Console.ReadLine();
+                        answer = answer.Trim().ToLower();
+                        if (answer.Equals("yes"))
+                        {
+                            Console.Write("Who is the new owner? ");
+                            book.Owner = Console.ReadLine();
+                            Console.WriteLine("The new owner is {0}.", book.Owner);
+                        }
+                        else if (answer.Equals("no"))
+                        {
+                            Console.WriteLine("Ok, {0} is still the owner.", book.Owner);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input, please try again.");
+                        }
+                        break;
+                    case "read":
+                        book.TotalTimesRead++;
+                        Console.WriteLine("The total times read is now {0}.", book.TotalTimesRead);
+                        break;
+                    case "print":
+                        book.Print();
+                        break;
+                    case "done":
+                        Console.WriteLine("Goodbye");
+                        isDone = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input, please try again.");
+                        break;
+                }
+            }
+            while (!isDone);
+        }
+    }
+}
