da2a1c7659ccd5ed3b12489e9a4f5f6d519fbdc1
Jonathan Benz <98258254+JonathanBenz@users.noreply.github.com>
2024-10-10 16:40:40 -0400

    PE_Inheritance_JRB

---
+++ PE/PE_Inheritance_JRB/DrawSquareItem.cs
@@ -0,0 +1,48 @@
+﻿// New child class by Jonathan Benz
+namespace PE_Inheritance_JRB
+{
+    internal class DrawSquareItem : MenuItem
+    {
+        // ~~~ FIELDS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        private int size;
+
+        // ~~~ PROPERTIES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // None specific to this child class
+
+        // ~~~ CONSTRUCTORS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public DrawSquareItem(string keyword, string description, string actionText)
+            : base(keyword, description, actionText)
+        {
+        }
+
+        // ~~~ OVERRIDES from Object ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // None specific to this child class
+
+        // ~~~ METHODS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        // The child class can do as much as it likes when overriding!
+        public override void Run()
+        {
+            // Setup
+            Console.WriteLine(actionText);
+
+            bool inputSuccessful = int.TryParse(Console.ReadLine(), out size);
+
+            if (inputSuccessful)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        Console.Write("? ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+    }
+}
---
+++ PE/PE_Inheritance_JRB/Program.cs
@@ -0,0 +1,67 @@
+﻿// Main program borrowed from DynamicMenus.ZIP File
+// Modified by Jonathan Benz
+
+namespace PE_Inheritance_JRB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ~~~ Variables we'll need + their initializations ~~
+            // Create some menu items. Yes, ideally, these would all be in an array so we could loop
+            // over them later, but they are all different types. We'll discuss how to make that
+            // work later.
+            MenuItem helloWorld = new MenuItem("Hello", "Say hello", "Hello GDAPS1!");
+            GetTimeItem getTime = new GetTimeItem();
+            GameItem game = new GameItem(1, "hard", new Random());
+            MenuItem drawSquare = new DrawSquareItem("Draw", "Draw a square", "What size square would you like drawn?");
+
+            // while true so the menu runs forever for demo purposes
+            // Use Ctrl-C to quit.
+            string choice = null;
+
+            // ~~~ The main "game" loop ~~~
+            while (choice != "QUIT")
+            {
+                // Print the menu
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine(helloWorld);
+                Console.WriteLine(getTime);
+                Console.WriteLine(drawSquare);
+                Console.WriteLine(game);
+                Console.WriteLine("\tQUIT: say goodbye.");
+
+                // Get and act on response
+                Console.Write("> ");
+                choice = Console.ReadLine().Trim().ToUpper();
+
+                // A switch would be ideal here, but can't be used with non-constant
+                // cases (i.e. it won't work with the Keyword property)
+                if (choice == helloWorld.Keyword)
+                {
+                    helloWorld.Run();
+                }
+                else if (choice == getTime.Keyword)
+                {
+                    getTime.Run();
+                }
+                else if (choice == game.Keyword)
+                {
+                    game.Run();
+                }
+                else if(choice == drawSquare.Keyword)
+                {
+                    drawSquare.Run();
+                }
+                else if (choice != "QUIT")
+                {
+                    Console.WriteLine("I don't know how to do that!");
+                }
+                // Otherwise, we're quitting, so do nothing and say goodbye once we exit the loop
+            }
+
+            // ~~~ All done ~~~
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
